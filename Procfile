b3d6b2868283c6c1badee8ef4476eda651d122f5
techboy237 <62163069+techboy237@users.noreply.github.com>
2022-04-14 07:24:08 -0700

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m Techboy237bot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Techboy237bot
+:: Enables virtual env mode and then starts Rika
+env\scripts\activate.bat && py -m Techboy237bot
