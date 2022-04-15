d73f0ffd2d813cb2191d3676bcbc4f13a4bb0368
techboy237 <62163069+techboy237@users.noreply.github.com>
2022-04-15 08:49:27 +0100

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Techboy237bot
-net start Techboy237bot
+net stop LaylaRobot
+net start LaylaRobot
